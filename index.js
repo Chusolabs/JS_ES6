804f5bfaa6b563b356e61d6e75ba4d6520877b5c
S0019589516 <j.pachecopatricio@accenture.com>
2018-11-08 09:01:21 +0000

    Comments and variables definition

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 console.log(typeof document);
 console.log(document);
-document.getElementById('example').innerHTML = 'Javascript Rules';
+//document.getElementById('example').innerHTML = 'Javascript Rules';
+
+// single line comment
+
+/*
+   multi 
+   line 
+   comment
+*/
+
+var a, b;
+a ='hello'; //asignment. we can use '' or "" to create a string
+b = a + 'world'; //b = 'Hello world';
+
+var c, d;
+c = 1;
+b = a + 5.5; //b= 6.5;
